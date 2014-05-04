a8681f67eb539182ff4ed1bd4cf27bae58e543a4
Dominik Piatek <dominik.piatek@econsultancy.com>
2014-05-04 13:18:28 +0100

    init

---
+++ js/app.js
@@ -0,0 +1,23 @@
+function load() {
+  gapi.client.setApiKey('AIzaSyAFiqMW3gEMzwckXdJ14bkduSB_kDHWULM');
+  gapi.client.load('youtube', 'v3', handleAPILoaded);
+}
+
+// After the API loads, call a function to enable the search box.
+function handleAPILoaded() {
+  $('#search-button').attr('disabled', false);
+}
+
+// Search for a specified string.
+function search() {
+  var q = $('#query').val();
+  var request = gapi.client.youtube.search.list({
+    q: q,
+    part: 'snippet'
+  });
+
+  request.execute(function(response) {
+    var str = JSON.stringify(response.result);
+    $('#search-container').html('<pre>' + str + '</pre>');
+  });
+}
