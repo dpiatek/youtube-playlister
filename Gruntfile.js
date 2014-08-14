1e7f7dd46ed4da3d481e6d80263aa212190c38e1
Dominik Piatek <dominik.piatek@econsultancy.com>
2014-08-14 19:27:08 +0100

    Add basic usemin setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,30 @@ module.exports = function(grunt) {
           'css/style.css': 'css/style.scss'
         }
       }
+    },
+    useminPrepare: {
+      html: 'index.html'
+    },
+    usemin: {
+      html: 'dist/index.html'
+    },
+    copy: {
+      build: {
+        dest: 'dist/index.html',
+        src: 'index.html'
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task(s).
   grunt.registerTask('default', ['sass']);
+  grunt.registerTask('build', ['copy', 'useminPrepare', 'concat', 'uglify', 'usemin']);
 
 };
