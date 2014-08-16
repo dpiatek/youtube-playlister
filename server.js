c78d2fead3decc5ea68ec90e5bf240493687da0f
Dominik Piatek <dominik.piatek@econsultancy.com>
2014-08-16 10:48:46 +0100

    Add logger

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var connect = require('connect'),
     http = require('http'),
     static = require('serve-static'),
+    logger = require('morgan'),
     port = process.env.PORT || 5000,
+    env = process.env.YT_PL_ENV || 'development',
     app = connect();
 
-app.use(static('dist'));
+if (env == 'production') {
+  app.use(logger('short'));
+  app.use(static('dist'));
+} else {
+  app.use(logger('dev'));
+  app.use(static('app'));
+}
+
 http.createServer(app).listen(port);
 console.log('Listening on http://localhost:' + port);
